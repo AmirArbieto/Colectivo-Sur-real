142f8ec3236b7163869904393638a6c81adf4288
AmirArbieto <amir.arbieto@vallegrande.edu.pe>
2023-01-20 18:52:43 -0500

    html?

--- js/script.js
+++ js/script.js
@@ -6,6 +6,23 @@
 ** Boton leer mas 
 ** Carrusel cards 
 ** Slider carrusel
+** Hypervinculos 
+** Actividades
+** Exposiciones
+** Proximos eventos
+** Proximos eventos card 1
+** Proximos eventos card 2
+** Proximos eventos card 3
+** Proximos eventos card 4
+** Proximos eventos card 5
+** Proximos eventos card 6
+** Cañete cultural
+** Noticias
+** Documentales
+** Podcast
+** Mas noticias
+** Libros 
+** Pie de pagina
 */
 
 
@@ -218,3 +235,270 @@ function slides(){
 
 
 
+// Hypervinculos
+// Menu 
+function Boton_Contactenos(){
+	window.open('/html/Contactenos.html', '_blank');
+}
+
+function Boton_Integrantes(){
+    location.href='/html/Integrantes.html';
+}
+
+function Boton_Proyectos_Historicos(){
+    location.href='/html/Proyectos_Historicos.html';
+}
+
+function Boton_Logeo(){
+    window.open('/html/Logeo.html', '_blank');
+}
+
+// Sub menu 
+function Boton_Colectivo_Sur_Real(){
+    location.href='/index.html';
+}
+
+function Boton_Actividades(){
+    location.href='/html/Actividades.html';
+}
+
+function Boton_Cañete_Cultural(){
+    location.href='/html/Cañete_Cultural.html';
+}
+
+function Boton_Noticias(){
+    location.href='/html/Noticias.html';
+}
+
+function Boton_Libros(){
+    location.href='/html/Libros.html';
+}
+
+
+
+
+
+
+
+
+
+
+
+// Actividades
+// Exposiciones
+function Exposiciones_1(){
+    window.open('https://www.google.com/search?client=opera-gx&q=exposiciones&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Exposiciones_2(){
+    window.open('https://www.google.com/search?client=opera-gx&q=exposiciones&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Exposiciones_3(){
+    window.open('https://www.google.com/search?client=opera-gx&q=exposiciones&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Exposiciones_4(){
+    window.open('https://www.google.com/search?client=opera-gx&q=exposiciones&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Exposiciones_5(){
+    window.open('https://www.google.com/search?client=opera-gx&q=exposiciones&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Exposiciones_6(){
+    window.open('https://www.google.com/search?client=opera-gx&q=exposiciones&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+
+
+
+
+
+
+
+
+
+
+// Proximos eventos
+function Proximos_Eventos_Mapa_1(){
+    window.open('https://www.google.com/maps/search/maps/@-12.5730429,-77.2411409,9z/data=!3m1!4b1', '_blank');
+}
+
+function Proximos_Eventos_Mapa_2(){
+    window.open('https://www.google.com/maps/search/maps/@-12.5730429,-77.2411409,9z/data=!3m1!4b1', '_blank');
+}
+
+function Proximos_Eventos_Mapa_3(){
+    window.open('https://www.google.com/maps/search/maps/@-12.5730429,-77.2411409,9z/data=!3m1!4b1', '_blank');
+}
+
+function Proximos_Eventos_Mapa_4(){
+    window.open('https://www.google.com/maps/search/maps/@-12.5730429,-77.2411409,9z/data=!3m1!4b1', '_blank');
+}
+
+function Proximos_Eventos_Mapa_5(){
+    window.open('https://www.google.com/maps/search/maps/@-12.5730429,-77.2411409,9z/data=!3m1!4b1', '_blank');
+}
+
+function Proximos_Eventos_Mapa_6(){
+    window.open('https://www.google.com/maps/search/maps/@-12.5730429,-77.2411409,9z/data=!3m1!4b1', '_blank');
+}
+
+//
+function Proximos_Eventos_Compartir(){
+	window.open('https://api.whatsapp.com/send?text=http://127.0.0.1:5500/Colectivo_Sur_Real/html/Actividades.html#&text=Acompáñame a divertirme en este evento', '_blank');
+}
+
+function Proximos_Eventos_Inscribirse(){
+    window.open('https://www.google.com/search?client=opera-gx&q=exposiciones&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+
+
+
+
+
+
+
+
+// Cañete cultural
+function Cards_Flip_Link_1(){
+    window.open('https://www.google.com/search?client=opera-gx&q=curiosidades&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Cards_Flip_Link_2(){
+    window.open('https://www.google.com/search?client=opera-gx&q=curiosidades&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Cards_Flip_Link_3(){
+    window.open('https://www.google.com/search?client=opera-gx&q=curiosidades&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Cards_Flip_Link_4(){
+    window.open('https://www.google.com/search?client=opera-gx&q=curiosidades&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Cards_Flip_Link_5(){
+    window.open('https://www.google.com/search?client=opera-gx&q=curiosidades&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Cards_Flip_Link_6(){
+    window.open('https://www.google.com/search?client=opera-gx&q=curiosidades&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+
+
+
+
+
+
+
+
+
+
+// Noticias 
+// Documentales
+function Noticias_Documentales_1(){
+	window.open('https://www.google.com/search?client=opera-gx&q=curiosidades&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Noticias_Documentales_2(){
+	window.open('https://www.google.com/search?client=opera-gx&q=noticias&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Noticias_Documentales_3(){
+	window.open('https://www.google.com/search?client=opera-gx&q=noticias&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+// Podcast 
+function Noticias_Podcast_1(){
+	window.open('https://www.google.com/search?client=opera-gx&q=noticias&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Noticias_Podcast_2(){
+	window.open('https://www.google.com/search?client=opera-gx&q=noticias&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Noticias_Podcast_3(){
+	window.open('https://www.google.com/search?client=opera-gx&q=noticias&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+// Mas noticias 
+function Noticias_Mas_Noticias_1(){
+	window.open('https://www.google.com/search?client=opera-gx&q=noticias&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Noticias_Mas_Noticias_2(){
+	window.open('https://www.google.com/search?client=opera-gx&q=noticias&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Noticias_Mas_Noticias_3(){
+	window.open('https://www.google.com/search?client=opera-gx&q=noticias&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+
+
+
+
+
+
+
+
+
+
+// Libros 
+function Libros_Comprar_Coaldas(){
+	window.open('https://www.google.com/search?client=opera-gx&q=comprar+libro+coaldas&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Libros_Comprar_Creacion_Politica_Cañete(){
+	window.open('https://www.google.com/search?client=opera-gx&q=comprar+libro+coaldas&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Libros_Comprar_Decimas(){
+	window.open('https://www.google.com/search?client=opera-gx&q=comprar+libro+coaldas&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Libros_Comprar_Estacion_San_Vicente(){
+	window.open('https://www.google.com/search?client=opera-gx&q=comprar+libro+coaldas&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Libros_Comprar_Esteban(){
+	window.open('https://www.google.com/search?client=opera-gx&q=comprar+libro+coaldas&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Libros_Comprar_Principio_Noser(){
+	window.open('https://www.google.com/search?client=opera-gx&q=comprar+libro+coaldas&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+function Libros_Comprar_Historia_Cañete(){
+	window.open('https://www.google.com/search?client=opera-gx&q=comprar+libro+coaldas&sourceid=opera&ie=UTF-8&oe=UTF-8', '_blank');
+}
+
+
+
+
+
+
+
+
+
+
+
+// Pie de pagina 
+function Boton_Facebook(){
+	window.open('https://www.facebook.com/colectivo.surreal.5/', '_blank');
+}
+
+function Boton_Github() {
+	window.open('https://eliseodesign.github.io', '_blank');
+}
+
+function Boton_Youtube(){
+	window.open('https://www.youtube.com/@fersareik', '_blank');
+}
+
+function Boton_Whatsapp(){
+	window.open('https://api.whatsapp.com/send?phone=954708490&text=Buenas tardes, me podrian brindar mas informacion sobre ustedes?', '_blank');
+}
